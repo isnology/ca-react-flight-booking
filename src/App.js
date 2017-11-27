0655494373fa360b6d7bd58946b525bdc284e47d
IsNology <glenn@isnology.com>
2017-11-27 11:21:18 +1100

    initial commit with button component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>this is a heading</h1> 
+        <Button title='Find Flights' primary={true} />
+        <Button title='Sign up' />
+        <Button title='Enter Competition' magic={true}/>
+        <ul>
+          <li>First</li>
+          <li>Second</li>
+          <li>Third</li>
+        </ul>
+        No more content
+      </div>
+    );
+  }
+}
+
+export default App;
